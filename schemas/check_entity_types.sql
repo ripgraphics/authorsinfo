56cd41038176df8745b7482b6807c107359cecab
ripgraphics <info@ripgraphics.com>
2025-09-07 11:40:38 -0400

    chore: remove obsolete SQL files related to metadata and engagement
    - Deleted several SQL scripts that were no longer in use, including those for adding metadata columns, updating activities, and managing engagement-related data.
    - Cleaned up legacy scripts to streamline the database management process and reduce clutter in the repository.
    - Ensured that the removal of these files does not affect current functionality or data integrity.

--- schemas/check_entity_types.sql
+++
@@ -1,6 +0,0 @@
--- Check what entity types currently exist in the database
-SELECT DISTINCT entity_type, COUNT(*) as count
-FROM activities 
-WHERE entity_type IS NOT NULL
-GROUP BY entity_type 
-ORDER BY count DESC;
--- update_user_album_images.sql
+++
@@ -1,9 +0,0 @@
--- Update images in user albums to set uploader_id to album owner
-UPDATE public.images 
-SET uploader_id = pa.entity_id,
-    uploader_type = 'user'
-FROM public.photo_albums pa
-WHERE public.images.album_id = pa.id
-  AND pa.entity_type = 'user'
-  AND public.images.uploader_id IS NULL
-  AND pa.entity_id IS NOT NULL;
